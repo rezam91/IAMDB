6244af5c5e16d2d6c0c118963408f6c4c0bbd686
Reza <r.mehralizadeh@gmail.com>
2025-08-29 18:54:26 +0330

    feat(Not Found): Not Found page developed

--- src/pages/notfound.jsx
+++ src/pages/notfound.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
     return (
-        <>
-            <div>Not Found</div>
-        </>
-    )
-}
+        <div className='text-white'>
+            <h1 className='text-[40px]'>404</h1>
+            <p className='text-[18px] mb-[20px]'>Oops! The page you're looking for doesn't exist.</p>
+            <Link to="/" className="p-[12px] bg-[#222C4F] rounded-[18px]">Go back to Home</Link>
+        </div>
+    );
+};
 
-export default NotFound
+export default NotFound;
