b9c2d69c43b564ba46f69543407048070f7d8eac
Reza <r.mehralizadeh@gmail.com>
2025-09-01 01:56:09 +0330

    feat(result): mobile version of result page developed

--- src/pages/result.jsx
+++ src/pages/result.jsx
@@ -66,12 +66,12 @@ const Result = () => {
   return (
     <>  
       {/* <img src={movie.images} alt={movie.images} width="1280px" className='absolute mt-[-50px] mx-[-180px] z-[-1]' /> */}
-      <div className="absolute w-[1280px] h-[380px] top-0 mt-[-50px] mx-[-180px] overflow-hidden z-[-1]">
+      <div className="absolute w-[1280px] h-[380px] top-0 mx-[-180px] overflow-hidden z-[-1] poster-box">
         {/* Background image */}
         <img
           src={movie.images}
           alt={movie.images}
-          className="absolute top-0 left-0 w-full h-full object-cover z-[-2]"
+          className="absolute top-0 left-0 w-full h-full object-cover z-[-2] poster"
         />
         {/* <div className="z-[-1] absolute w-full h-[380px] bg-gradient-to-b from-[#070D23]/0 via-[#070D23]/70 via-[#070D23]/90 to-[#070D23]"></div> */}
         <div
@@ -88,56 +88,57 @@ const Result = () => {
       <div onClick={() => navigate(-1)} className='p-[10px] w-fit bg-[#222C4F] rounded-full cursor-pointer z-20'>
         <img src={backSignPath} alt="" width='20px' />
       </div>
-      <div className="mt-[100px] flex gap-[70px] text-white">
-        <div>
+      <div className="mt-[100px] flex gap-[70px] text-white description">
+        <div className='left-side'>
           <img
             src={movie.poster}
             alt={movie.title}
-            className="rounded-[18px]"
+            className="rounded-[18px] movie-art"
           />
+          <div className='rate-box'>
+            <div className='flex items-center gap-[18px] mt-[30px] circle-box'>
+              {/* Rating Circle */}
+              <div className="relative w-[80px] h-[80px]">
+                <svg className="absolute top-0 left-0 w-full h-full">
+                  {/* Empty background circle */}
+                  <circle
+                    cx="40"
+                    cy="40"
+                    r="35"
+                    stroke="#222C4F"
+                    strokeWidth="6"
+                    fill="none"
+                  />
+                  {/* Filled progress circle */}
+                  <circle
+                    cx="40"
+                    cy="40"
+                    r="35"
+                    stroke="#724CF9"
+                    strokeWidth="6"
+                    fill="none"
+                    strokeDasharray="220"
+                    strokeDashoffset={220 - (circleDegree / 360) * 220}
+                    strokeLinecap="round"
+                    transform="rotate(-90 40 40)"
+                  />
+                </svg>
+                {/* Rating text */}
+                <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center text-white font-[800] text-[24px]">
+                  {movie.imdb_rating}
+                </div>
+              </div>
 
-          <div className='flex items-center gap-[18px] mt-[30px]'>
-            {/* Rating Circle */}
-            <div className="relative w-[80px] h-[80px]">
-              <svg className="absolute top-0 left-0 w-full h-full">
-                {/* Empty background circle */}
-                <circle
-                  cx="40"
-                  cy="40"
-                  r="35"
-                  stroke="#222C4F"
-                  strokeWidth="6"
-                  fill="none"
-                />
-                {/* Filled progress circle */}
-                <circle
-                  cx="40"
-                  cy="40"
-                  r="35"
-                  stroke="#724CF9"
-                  strokeWidth="6"
-                  fill="none"
-                  strokeDasharray="220"
-                  strokeDashoffset={220 - (circleDegree / 360) * 220}
-                  strokeLinecap="round"
-                  transform="rotate(-90 40 40)"
-                />
-              </svg>
-              {/* Rating text */}
-              <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center text-white font-[800] text-[24px]">
-                {movie.imdb_rating}
+              <div className='flex flex-col'>
+                <span className='text-[16px] font-[700] opacity-[0.8]'>{movie.imdb_votes}</span>
+                <span className='text-[14px] font-[400] opacity-[0.6]'>ratings on IMDB</span>
               </div>
             </div>
 
-            <div className='flex flex-col'>
-              <span className='text-[16px] font-[700] opacity-[0.8]'>{movie.imdb_votes}</span>
-              <span className='text-[14px] font-[400] opacity-[0.6]'>ratings on IMDB</span>
+            {/* Ratings Output */}
+            <div className="mt-[30px] text-[13px] font-[400] opacity-[0.4] leading-[24px] other-rating">
+              {filteredRatings}
             </div>
-          </div>
-
-          {/* Ratings Output */}
-          <div className="mt-[30px] text-[13px] font-[400] opacity-[0.4] leading-[24px]">
-            {filteredRatings}
           </div>
         </div>
 
@@ -146,7 +147,7 @@ const Result = () => {
             <div className='flex justify-between items-center'>
               <h1 className="text-[48px] font-[700] mx-0 my-0">{movie.title}</h1>
               <div
-                className="relative w-[24px] h-[24px] cursor-pointer"
+                className="relative w-[24px] h-[24px] cursor-pointer like-box"
                 onClick={(e) => {
                   e.stopPropagation(); // prevent accidental navigation
                   toggleLike(movie.id);
@@ -175,8 +176,8 @@ const Result = () => {
               <span className='px-[12px] py-[6px] rounded-[8px] bg-[#222C4F]'>{movie.year}</span>
               <span className='px-[12px] py-[6px] rounded-[8px] bg-[#222C4F] flex items-center gap-[6px]'><img src={clockPath} alt="clock" width="12px" />{movie.runtime}</span>
             </div>
-            <h2 className='mt-[18px] text-[28px] font-[700] leading-[50px]'>Details</h2>
-            <ul className="text-sm text-white space-y-2">
+            <h2 className='mt-[18px] text-[28px] font-[700] leading-[50px] detail-p'>Details</h2>
+            <ul className="text-sm text-white space-y-2 movie-description">
               <li className="py-[12px] border-b border-[#222C4F] flex">
                 <span className="w-32 font-[700] text-[16px] opacity-[0.8]">Directors</span>
                 <span className='opacity-[0.7] font-[400] text-[14px]'>{movie.director}</span>
